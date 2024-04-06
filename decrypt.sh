a0e5680ce4a1191c97e1c3970356463867fc9e1c
Doridian <git@doridian.net>
2024-04-06 11:32:21 -0700

    fixup

--- decrypt.sh
+++ decrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 set -x
-cd /opt/keys
+cd /opt/icefox
 source ./shared.sh
 
 KEYNAME="$1"
 
-aws kms decrypt --key-id "$(cat /opt/keys/key.id)" --ciphertext-blob "fileb:///opt/keys/$KEYNAME" | jq -r .Plaintext | base64 -d > "/mnt/keydisk/$KEYNAME"
+aws kms decrypt --key-id "$(cat /opt/icefox/key.id)" --ciphertext-blob "fileb:///opt/keys/$KEYNAME" | jq -r .Plaintext | base64 -d > "/mnt/keydisk/$KEYNAME"
--- encrypt.sh
+++ encrypt.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 set -x
-cd /opt/keys
+cd /opt/icefox
 source ./shared.sh
 
 KEYNAME="$1"
 
-aws kms encrypt --key-id "$(cat /opt/keys/key.id)" --plaintext "fileb:///mnt/keydisk/$KEYNAME" | jq -r .CiphertextBlob | base64 -d > "/opt/keys/$KEYNAME"
+mkdir -p /opt/keys
+
+aws kms encrypt --key-id "$(cat /opt/icefox/key.id)" --plaintext "fileb:///mnt/keydisk/$KEYNAME" | jq -r .CiphertextBlob | base64 -d > "/opt/keys/$KEYNAME"
