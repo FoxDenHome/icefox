bdb2948770e1d3e2e883aea002b3526b0377bbe8
Doridian <git@doridian.net>
2024-04-07 21:43:54 -0700

    Add backup stuff

---
+++ backup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/opt/icefox/restic.sh /opt/backup/run.sh /var/lib/docker/*/volumes /
---
+++ restic.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+runx() {
+    export REPO="$1"
+    export CMD="$2"
+    shift 2
+    "$CMD" "$@"
+}
+
+#runx 's3:s3.us-west-001.backblazeb2.com/icefox-backups' "$@"
+runx '/mnt/ztank/local/restic' "$@"
