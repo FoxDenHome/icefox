c8d0038f8b5b72650e08a8b479ea7209469aae7e
Doridian <git@doridian.net>
2024-08-24 11:18:24 -0700

    a

--- set-docker-tc.sh
+++ set-docker-tc.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 nsenter --net="/proc/$(docker inspect --format '{{.State.Pid}}' nas-web-1)/ns/net" tc qdisc add dev eth0 root tbf burst 1mb rate 500mbit latency 50ms >/dev/null 2>/dev/null
+
+exit 0
