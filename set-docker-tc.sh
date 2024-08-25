0d38a2b1e15b4fd65b3d534aedd502f7749464df
Doridian <git@doridian.net>
2024-08-24 23:15:23 -0700

    move

--- set-docker-tc.sh
+++ set-docker-tc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-nsenter --net="/proc/$(docker inspect --format '{{.State.Pid}}' nas-web-1)/ns/net" tc qdisc add dev eth0 root tbf burst 1mb rate 500mbit latency 50ms >/dev/null 2>/dev/null
+nsenter --net="/proc/$(docker inspect --format '{{.State.Pid}}' archmirror-rsync-1)/ns/net" tc qdisc add dev eth0 root tbf burst 1mb rate 500mbit latency 50ms >/dev/null 2>/dev/null
+nsenter --net="/proc/$(docker inspect --format '{{.State.Pid}}' archmirror-web-1)/ns/net" tc qdisc add dev eth0 root tbf burst 1mb rate 500mbit latency 50ms >/dev/null 2>/dev/null
 
 exit 0
